21be64c542ee724f5d1862ade5c252d3ec429144
nargok <nargok@gmail.com>
2019-03-23 13:41:53 +0900

    Firebaseとの通信

--- pages/axios.vue
+++ pages/axios.vue
@@ -2,28 +2,9 @@
    <section class="container">
      <h1>{{title}}</h1>
      <p>{{message}}</p>
-     <div>
-       <input type="text" v-model="msg" />
-       <button @click="doClick">Click</button>
-     </div>
-     <table>
-       <tr>
-         <th>User ID</th>
-         <td>{{json_data.userId}}</td>
-       </tr>
-       <tr>
-         <th>ID</th>
-         <td>{{json_data.id}}</td>
-       </tr>
-       <tr>
-         <th>Title</th>
-         <td>{{json_data.title}}</td>
-       </tr>
-       <tr>
-         <th>Body</th>
-         <td>{{json_data.body}}</td>
-       </tr>
-     </table>
+     <ul v-for="(data, key) in json_data">
+       <li>{{data.name}} ({{data.age}}) [{{key}}] </li>
+     </ul>
    </section>
 </template>
 
@@ -31,32 +12,31 @@
   import axios from 'axios';
 
 
-  let url = "https://jsonplaceholder.typicode.com/posts/";
+  const url = "https://vue-sample-baa0f.firebaseio.com/person.json";
 
   export default {
     data: function () {
       return {
         title: 'Axios',
-        msg: '',
         message: 'axios sample.',
-        json_data: {},
       };
     },
-    methods: {
-      doClick: function (event) {
-        axios.get(url + this.msg).then(res => {
-          this.message = 'get ID=' + this.msg;
-          this.json_data  = res.data;
-        }).catch((error) => {
-          this.message = error.message;
-          this.json_data = {};
-        });
-      },
-    },
+    asyncData: async function() {
+      const result = await axios.get(url);
+      return { json_data: result.data };
+    }
   }
 </script>
 
 <style>
+ul {
+  margin: 0 10px;
+  background-color: aliceblue;
+}
+li {
+  padding: 10px;
+  font-size: 16pt;
+}
 input {
   font-size: 14pt;
 }
